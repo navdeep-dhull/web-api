195897ce2202c56c275d97acf5422e65f1886d71
Navdeep Kumar <navdeepk@HYTECH.IN>
2016-12-09 14:51:07 +0530

    first add

---
+++ Web-api-test/Web-api-test/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Web_api_test.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
